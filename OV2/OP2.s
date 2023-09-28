65cc5d4e176ef097b2ffb6164a8079834e9d8e9a
Eirik <eirikmathias@gmail.com>
2023-09-28 20:14:55 +0200

    Bugfix

--- OV2/OP2.s
+++ OV2/OP2.s
@@ -1,6 +1,6 @@
 #A program that will take the input register a0-a5 add together two and two to get the value of a pair
 #Then compare the numbers and put the largest pair in a0 regestry
-li a0, 2
+li a0, 29
 li a1, 2
 li a2, 4
 li a3, 5
@@ -14,13 +14,12 @@ add a4, a5, a4
 
 #Check if a2 register is bigger than a0
 #if correct move a2 value to a0 register
-ble a2, a0, hvis1
-hvis1:
+foo:
+    bge a0, a2, _else
     mv a0, a2
-
-#Check if a4 register is bigger than a0
-#if correct move a4 value to a0 register
-#This will also be checked against a2 register if needed
-ble a4, a0, hvis2
-hvis2: 
+_else:
+    ble a4, a0, _end
     mv a0,a4
+_end: 
+    nop
+    
